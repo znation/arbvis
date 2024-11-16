16ab704529f7620f71b73df04da4ec5401124150
Zach Nation <zach@xetdata.com>
2024-11-16 02:20:28 -0800

    add file input and color scheme

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
+use std::io::{self, Read};
+
 use image::{DynamicImage, GenericImage, Rgba};
 use show_image::create_window;
 
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let w: usize = 1280;
+    let h: usize = 720;
 
-    // output
-    let w= 1280;
-    let h = 720;
-    let mut img = DynamicImage::new_rgb8(w, h);
+    // create input image (same size as output)
+    // for now, truncate at output size using raw bytes
+    let mut input = Vec::<u8>::new();
+    input.reserve(w as usize * h as usize);
+    let mut count = 0;
+    for possible_value in io::stdin().bytes() {
+        if count >= w * h {
+            break;
+        }
+        match possible_value {
+            Ok(v) => input.push(v),
+            Err(e) => return Err(Box::new(e)),
+        }
+        count += 1;
+    }
+
+    // create output image
+    let mut img = DynamicImage::new_rgb8(w as u32, h as u32);
     for x in 0..w {
         for y in 0..h {
-            let r = 0;
-            let g = 0;
-            let b = ((x as f32 * y as f32) / (w as f32 * h as f32)) * 255.0;
-            let pixel = Rgba::<u8>([r, g, b as u8, 255]);
-            img.put_pixel(x, y, pixel);
+            let mut pixel = Rgba::<u8>([0, 0, 0, 255]);
+
+            // color scheme from
+            // https://stairwell.com/resources/hilbert-curves-visualizing-binary-files-with-color-and-patterns/
+            let i = (x * y) % input.len();
+            if input[i] == 0 {
+                // null (0) bytes
+                // do nothing; default color is black
+            }
+            else if input[i] <= 0x1F {
+                // low bytes/control characters
+                pixel = Rgba::<u8>([0, 255, 0, 255]);
+            }
+            else if input[i] <= 0x7E {
+                // ascii printable chars
+                pixel = Rgba::<u8>([0, 0, 255, 255]);
+            }
+            else {
+                // high bytes
+                pixel = Rgba::<u8>([255, 0, 0, 255]);
+            }
+            
+            img.put_pixel(x as u32, y as u32, pixel);
         }
     }
 
